5c6bd26d687caf4c12dc2f434feeaddcea044814
xiaban <980892894@qq.com>
2024-07-15 16:31:36 +0800

    refactor: 更改项目部分结构

--- dynamic-remotes/app1/src/bootstrap.js
+++
@@ -1,5 +0,0 @@
-import App from './App';
-import React from 'react';
-import {createRoot} from "react-dom/client";
-
-createRoot(document.getElementById('root')).render(<App/>);
--- dynamic-remotes/app2/src/index.js
+++ dynamic-remotes/app2/src/index.js
@@ -1,1 +1,5 @@
-import('./bootstrap');
+import App from './App';
+import React from 'react';
+import {createRoot} from "react-dom/client";
+
+createRoot(document.getElementById('root')).render(<App/>);
