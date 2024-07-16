7150c2ced40f7516b3a9d3bc1cc3f91b1357cc8a
xiaban <980892894@qq.com>
2024-07-16 10:23:12 +0800

    perf: 优化代码结构

--- dynamic-remotes/app1/src/App.js
+++ dynamic-remotes/app1/src/App.js
@@ -37,6 +37,7 @@ function App() {
             remotes: remotes,
         });
     }, [remotes]);
+
     const setApp2 = () => {
         setSystem({
             scope: 'app2',
@@ -57,7 +58,7 @@ function App() {
             module: 'Other',
         });
     };
-    const setR = () => {
+    const registerAPP2 = () => {
         setRemotes([
             {
                 name: 'app2',
@@ -70,14 +71,12 @@ function App() {
 
     return (
         <div>
-            <button onClick={setR}>
-                点击
-            </button>
+            <button onClick={registerAPP2}>注册APP2</button>
             <h1>宿主机</h1>
             <div>这是一个宿主机</div>
-            <button onClick={setApp2}>加载App2的Widget</button>
-            <button onClick={setApp3}>加载App3的Widget</button>
-            <button onClick={setApp3_2}>加载App3的Other</button>
+            <button onClick={setApp2}>加载APP2的Widget</button>
+            <button onClick={setApp3}>加载APP3的Widget</button>
+            <button onClick={setApp3_2}>加载APP3的Other</button>
             <div style={{ marginTop: '2em' }}>
                 <Suspense fallback="加载中">{Component ? <Component /> : null}</Suspense>
             </div>
--- dynamic-remotes/app2/src/App.js
+++ dynamic-remotes/app2/src/App.js
@@ -1,10 +1,8 @@
-import LocalButton from './Widget';
 import React from 'react';
 const App = () => (
     <div>
         <h1>Dynamic System Host</h1>
         <h2>App 2</h2>
-        <LocalButton />
     </div>
 );
 
